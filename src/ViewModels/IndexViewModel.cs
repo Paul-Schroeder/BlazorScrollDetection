8c7d54b4f914db2b2f16a4edbb9b99dd4f4d7c1e
Paul-Schroeder <paul@msctek.com>
2021-06-01 10:55:58 -0500

    initial commit for Glenn

---
+++ src/ViewModels/IndexViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorScrollDetection.ViewModels
+{
+    public class IndexViewModel : ComponentBase
+    {
+        protected string result;
+        private DotNetObjectReference<IndexViewModel> _objRef;
+        protected bool IsScrollTrackingEnabled { get; set; } = false;
+
+        [Inject]
+        protected IJSRuntime JS { get; set; }
+
+        public void Dispose()
+        {
+            _objRef?.Dispose();
+        }
+
+        [JSInvokable]
+        public bool IsAtWindowBottom(int windowScrollTop, int windowHeight, int documentHeight)
+        {
+            bool retVal = (windowScrollTop + windowHeight) == (documentHeight);
+            return retVal;
+        }
+
+        [JSInvokable]
+        public bool IsNearWindowBottom(int windowScrollTop, int windowHeight, int documentHeight)
+        {
+            bool retVal = (windowScrollTop + windowHeight) > (documentHeight - 100);
+            return retVal;
+        }
+
+        public async Task ToggleDotNetTrackScroll()
+        {
+            _ = await JS.InvokeAsync<string>("blazorExtensions.toggleTrackScroll", _objRef);
+        }
+
+        [JSInvokable]
+        public bool ToggleTrackScroll() { IsScrollTrackingEnabled = !IsScrollTrackingEnabled; return IsScrollTrackingEnabled; }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _objRef = DotNetObjectReference.Create(this);
+                await JS.InvokeVoidAsync("blazorExtensions.toggleTrackScroll", _objRef);
+                StateHasChanged();
+            }
+        }
+    }
+}
---
+++ src/wwwroot/js/scripts.js
@@ -0,0 +1,46 @@
+﻿var _dotNetObjRef;
+
+window.blazorExtensions = {
+    toggleTrackScroll: function (dotNetObjRef) {
+        _dotNetObjRef = dotNetObjRef;
+
+        dotNetObjRef.invokeMethodAsync('ToggleTrackScroll')
+            .then(isScrollTrackingEnabled => {
+                console.log('isScrollTrackingEnabled: ' + isScrollTrackingEnabled);
+                if (isScrollTrackingEnabled) {
+                    blazorExtensions.enableScrollTracking();
+                } else {
+                    blazorExtensions.disableScrollTracking();
+                }
+            });
+    },
+    enableScrollTracking: function () {
+        console.log('enableScrollTracking invoked');
+        window.addEventListener("scroll", blazorExtensions.handleWindowScroll);
+    },
+    disableScrollTracking: function () {
+        console.log('disableScrollTracking invoked');
+        window.removeEventListener("scroll", blazorExtensions.handleWindowScroll);
+    },
+    handleWindowScroll: function () {
+        console.log('$(window).scrollTop(): ' + $(window).scrollTop());
+        console.log('$(window).height(): ' + $(window).height());
+        console.log('$(document).height(): ' + $(document).height());
+
+        _dotNetObjRef.invokeMethodAsync('IsNearWindowBottom', $(window).scrollTop(), $(window).height(), $(document).height())
+            .then(isNearWindowBottom => {
+                console.log('isNearWindowBottom: ' + isNearWindowBottom);
+                if (isNearWindowBottom) {
+                    alert("You are NEAR the bottom!");
+                }
+            });
+
+        _dotNetObjRef.invokeMethodAsync('IsAtWindowBottom', $(window).scrollTop(), $(window).height(), $(document).height())
+            .then(isAtWindowBottom => {
+                console.log('isAtWindowBottom: ' + isAtWindowBottom);
+                if (isAtWindowBottom) {
+                    alert("You are AT the bottom!");
+                }
+            });
+    }
+};
